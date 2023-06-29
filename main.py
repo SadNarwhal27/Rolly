41dca88c7910a20e9d26471ee6a20c7e23a3da14
SadNarwhal27 <schallt2015@gmail.com>
2023-06-29 15:50:01 -0400

    Refactoring everything

--- main.py
+++ main.py
@@ -1,55 +1,92 @@
-import os, discord
+import os
+from typing import Optional
+import discord
 from dotenv import load_dotenv
-from dice import get_rolls, get_dice
-from checks import get_response
-from utils import embed_maker, six_nine
-import admin
+from enum import Enum
+
+import dice
 
 load_dotenv()
-
-token = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+class bot_client(discord.Client):
+    
+    def __init__(self):
+        """Initiliazes intents and synced settings across bot instances"""
+
+        super().__init__(intents=intents)
+        self.synced = False # Syncs views only once
+        self.added = False # Syncs command tree only once
+
+    async def on_ready(self):
+        """Syncs settings across different instances of the bot"""
+
+        await self.wait_until_ready()
+
+        if not self.added:
+            self.add_view(TestView())
+            self.addded = True
+
+        # Checks if tree commands have been synced
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
+
+        print(f"Say hi to {self.user}")
+
+client = bot_client()
+tree = discord.app_commands.CommandTree(client)
+
+class TestView(discord.ui.View):
+
+    def __init__(self) -> None:
+        super().__init__(timeout=30)
+
+    @discord.ui.button(custom_id='roll_button', label='Roll Again', style=discord.ButtonStyle.blurple)
+    async def roll_again(self, interaction: discord.Interaction, button: discord.ui.Button):
+        embed = discord.Embed(
+        title=f'Looks like you got a {dice.roll_dice(1, 20)[0]}', 
+        color=0xff0000,)
+        embed.set_author(name=interaction.user.global_name, icon_url=interaction.user.display_avatar)
+        await interaction.response.send_message(embed=embed, view=TestView())
 
 
-@client.event
-async def on_ready():
-    print(f"{client.user} has connected to Discord!")
+# Simgle command that can be called with /roll
+@tree.command(name='roll', description='Roll a die.', guild=discord.Object(os.getenv("DISCORD_GUILD")))
+async def roll(interaction: discord.Interaction):
+    embed = discord.Embed(
+        title=f'Looks like you got a {dice.roll_dice(1, 20)[0]}', 
+        color=0xff0000,)
+    embed.set_author(name=interaction.user.global_name, icon_url=interaction.user.display_avatar)
+    await interaction.response.send_message(embed=embed, view=TestView())
+
+# # Command with options at the end
+# GreetingTime = Enum(value='GreetingTime', names=['MORNING', 'AFTERNOON', 'EVENING', 'NIGHT'])
+# @tree.command(description='Respond according to the period of the day.', guild=discord.Object(os.getenv("DISCORD_GUILD")))
+# @discord.app_commands.describe(period='Period of the day')
+# async def greet_user_time_of_the_day(interaction: discord.Interaction, period: GreetingTime):
+#     user = interaction.user.id
+#     await interaction.response.send_message(f'Good {period.name.title()}, <@{user}>!')
+#     return
 
 
 @client.event
 async def on_message(message):
+
+    # Checks if the message is from the bot itself to prevent spamming
     if message.author == client.user:
         return
+    
+    if message.content == 'Hello!':
+        await message.channel.send("Hello there!")
 
-    if '$roll' in message.content:
-        await message.channel.send(embed=roll(message))
+    if message.content == '$roll':
+        embed = discord.Embed(
+        title=f'Looks like you got a {dice.roll_dice(1, 20)[0]}', 
+        color=0xff0000,)
+        embed.set_author(name=client.user.global_name, icon_url=client.user.display_avatar)
+        await message.channel.send(embed=embed, view=TestView())
 
-    if message.content == '$69':
-        await message.channel.send(six_nine())
-
-    if '$fill_db' in message.content:
-        await message.channel.send(admin.db_fill(message.content.replace('$fill_db ', '')))
-
-
-def roll(ctx):
-    # Starts assembling a response
-    text = get_response() + ' '
-    dice, sides = get_dice(ctx.content.strip('$roll '))
-    total = get_rolls('', None, dice, sides)
-    modifier = ''
-    simple = False
-    quip = ''
-    gif = ''
-
-    embed = embed_maker(text, total, 
-                        ctx.author.display_name,
-                        ctx.author.avatar, 
-                        quip, gif)
-    return embed
-
-
-client.run(token)
+client.run(os.environ.get('DISCORD_TOKEN'))
